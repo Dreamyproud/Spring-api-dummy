cc939f406deedfa26b7544af6cc1bbb462a95b76
Angela Garcia <danigarcia_1@hotmail.com>
2020-09-06 17:06:43 -0500

    Add delete and update

--- src/main/java/com/angela/apidummy/rest/PersonaController.java
+++ src/main/java/com/angela/apidummy/rest/PersonaController.java
@@ -10,8 +10,11 @@ import com.angela.apidummy.service.PersonaService;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +44,34 @@ public class PersonaController {
             response.put("code", "OK");
             response.put("message", "Se guardó correctamente el registro");
         } catch (Exception e) {
+            response.put("code", "FAIL");
+            response.put("message", e);
+        }
+        return response;
+    }
+    
+    @PutMapping("/updateById/{id}")
+    public JSONObject updateById(@PathVariable("id")Integer id, @RequestBody PersonaDto personaDtoU){
+        JSONObject response = new JSONObject();
+        try{
+           personaService.update(personaDtoU, id);
+           response.put("code", "Ok");
+           response.put("message", "Se actualizó correctamente");
+        }catch(Exception e){
+            response.put("code", "FAIL");
+            response.put("message", e);
+        }
+        return response;
+    }
+    
+    @DeleteMapping("/deleteById/{id}")
+    public JSONObject deleteById(@PathVariable("id")Integer id){
+        JSONObject response = new JSONObject();
+        try{
+           personaService.delete(id);
+           response.put("code", "Ok");
+           response.put("message", "Se ha eliminado correctamente");
+        }catch(Exception e){
             response.put("code", "FAIL");
             response.put("message", e);
         }
--- src/main/java/com/angela/apidummy/service/PersonaService.java
+++ src/main/java/com/angela/apidummy/service/PersonaService.java
@@ -9,6 +9,7 @@ import com.angela.apidummy.dto.PersonaDto;
 import com.angela.apidummy.model.Persona;
 import com.angela.apidummy.repository.PersonaRepository;
 import java.util.List;
+import java.util.Optional;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,27 +42,74 @@ public class PersonaService {
 
         return jsonArray;
     }
-    
-    public void create(PersonaDto personaDto) throws Exception{
-    
-        if(personaDto == null){
+
+    public void create(PersonaDto personaDto) throws Exception {
+
+        if (personaDto == null) {
             throw new Exception("La entidad persona está vacía.");
         }
-        
-        if(personaDto.getFullname().isEmpty()){
+
+        if (personaDto.getFullname().isEmpty()) {
             throw new Exception("El nombre está vacio");
         }
-        
+
         Persona persona = new Persona();
         persona.setFullname(personaDto.getFullname());
         persona.setEmail(personaDto.getEmail());
-        
-        try{
+
+        try {
             personaRepository.save(persona);
-        }catch(Exception e){
-            throw new Exception("Hubo un error registrando la persona. Error: "+e.getMessage());
+        } catch (Exception e) {
+            throw new Exception("Hubo un error registrando la persona. Error: " + e.getMessage());
         }
+
+    }
+
+    public void update(PersonaDto personaDtoU, Integer id) throws Exception {
+        if (id == null) {
+            throw new Exception("El usuario no existe");
+        }
+
+        if (personaDtoU == null) {
+            throw new Exception("No has ingresado datos");
+        }
+
+        Persona personaBD = findById(id);
+
+        if (personaBD == null) {
+            throw new Exception("El usuario no existe");
+        }
+
+        Persona person = new Persona();
+        person.setId(id);
+        person.setFullname(personaDtoU.getFullname() == null ? personaBD.getFullname() : personaDtoU.getFullname());
+        person.setEmail(personaDtoU.getEmail() == null ? personaBD.getEmail() : personaDtoU.getEmail());
+
+        try {
+            personaRepository.save(person);
+        } catch (Exception e) {
+            throw new Exception("Hubo un error actualizando. Error: " + e.getMessage());
+        }
+    }
+
+    public void delete(Integer id) throws Exception {
+        Persona IdDelete = findById(id);
         
+        System.out.println("delete: "+IdDelete);
+
+        if (IdDelete == null) {
+            throw new Exception("El usuario no existe");
+        }
+
+        try {
+            personaRepository.delete(IdDelete);
+        } catch (Exception e) {
+            throw new Exception("No se ha podido eliminar. Error " + e.getMessage());
+        }
+    }
+
+    public Persona findById(Integer id) {
+        return personaRepository.getOne(id);
     }
 
 }
